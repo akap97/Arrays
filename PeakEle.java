c7f547527095392f6cfabd47e5da3df2e9404507
akap97 <59849316+akap97@users.noreply.github.com>
2020-06-24 19:04:49 +0530

    Create PeakEle.java

---
+++ PeakEle.java
@@ -0,0 +1,22 @@
+class Peak
+{
+	// Function to find the peak element
+	// a[]: input array
+	// n: size of array a[]
+	public int peakElement(int[] arr,int n)
+    {
+       //add code here.
+       int l=0,h=n-1;
+       while(l<=h)
+       {
+           int mid=l+(h-l)/2;
+           if ((mid == 0 || arr[mid - 1] <= arr[mid]) && (mid == n - 1 || arr[mid + 1] <= arr[mid])) 
+                return mid; 
+            else if (mid > 0 && arr[mid - 1] > arr[mid]) 
+                 h=mid - 1;
+            else
+                l=mid+1;
+       }
+       return -1;
+    }
+}
